83c2d1499059e5ef27810c7d3ebb8f0bbff1040a
bndnt <67003594+bndnt@users.noreply.github.com>
2024-07-15 22:03:32 -0400

    mob+tablet

--- src/main.js
+++ src/main.js
@@ -37,3 +37,11 @@ document.querySelectorAll('.list').forEach(list => {
     });
   });
 });
+document
+  .querySelector('#mobile_menu_toggle')
+  .addEventListener('click', function (e) {
+    e.preventDefault();
+    this.classList.toggle('active');
+    document.querySelector('.top__hamburger').classList.toggle('active');
+    document.body.classList.toggle('hidden');
+  });
